4755fc98cd856b6f7964ceb18309e503ec3e993a
Ahmed Samy <ahmedsamyop100@gmail.com>
2024-04-25 20:03:31 +0200

    update title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getVideosBySearch } from "../../app/videosSlice";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getVideosBySearch } from '../../app/videosSlice'
 
-import SideBar from "../../components/sidebar/SideBar";
-import Video from "../../components/video/Video";
-import Loading from "../../components/loading/Loading";
-import "./home.css";
-import ErrorFetch from "../../components/errorFetch/ErrorFetch";
-import { Helmet } from "react-helmet";
+import SideBar from '../../components/sidebar/SideBar'
+import Video from '../../components/video/Video'
+import Loading from '../../components/loading/Loading'
+import './home.css'
+import ErrorFetch from '../../components/errorFetch/ErrorFetch'
+// import { Helmet } from "react-helmet";
 function Home() {
-  const dispatch = useDispatch();
-  const { videos, isLoading, error } = useSelector((state) => state.videos);
+  const dispatch = useDispatch()
+  const { videos, isLoading, error } = useSelector((state) => state.videos)
 
   useEffect(() => {
     if (!videos.data) {
-      dispatch(getVideosBySearch("القران"));
+      dispatch(getVideosBySearch('القران'))
     }
-  }, [dispatch, videos.data]);
+  }, [dispatch, videos.data])
   return (
     <>
-      <Helmet>
+      {/* <Helmet>
         <title>You!</title>
-      </Helmet>
+      </Helmet> */}
       <div className="home">
         <SideBar />
         <div className="videos">
@@ -29,7 +29,7 @@ function Home() {
           <div className="wrapper">
             {isLoading && <Loading />}
             {videos?.data
-              ?.filter((v) => v.type === "video")
+              ?.filter((v) => v.type === 'video')
               .map((video) => {
                 return (
                   <Video
@@ -43,13 +43,13 @@ function Home() {
                     views={video.viewCount}
                     date={video.publishedText}
                   />
-                );
+                )
               })}
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
